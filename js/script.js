566d38c6a29671a6c68e94f7088f9e3b415de848
“Nicole <nicoledicochea.dev@gmail.com>
2023-09-08 17:19:04 -0700

    create random generator structure

--- js/script.js
+++ js/script.js
@@ -11,11 +11,46 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  {
+    quote: '1',
+    source: 'a',
+    citation: 'book',
+    year: 1999,
+    tags: 'humor'
+  },
+  {
+    quote: '2',
+    source: 'b',
+    tags: 'humor'
+  },
+  {
+    quote: '3',
+    source: 'c',
+    tags: 'poetry'
+  },
+  {
+    quote: '4',
+    source: 'd',
+    tags: 'science'
+  },
+  {
+    quote: '5',
+    source: 'e',
+    tags: 'film'
+  }
+]
 
 
 /***
  * `getRandomQuote` function
 ***/
+
+function getRandomQuote( quotes ) {
+  const randomNum = Math.floor(Math.random() * (quotes.length))
+  return quotes[randomNum]
+}
+// console.log( getRandomQuote ( quotes ) )
 
 
 
@@ -23,6 +58,31 @@ project 1 - A Random Quote Generator
  * `printQuote` function
 ***/
 
+function printQuote() {
+  const randomQuote = getRandomQuote( quotes )
+  console.log(randomQuote)
+  let html = ''
+  html += `
+    <p class="quote">${randomQuote.quote}</p>
+    <p class="source">${randomQuote.source}
+  `
+  if( randomQuote.citation) {
+    html += `<span class="citation">${randomQuote.citation}</span>`
+  } 
+  if ( randomQuote.year ) {
+    html += `<span class="year">${randomQuote.year}</span>`
+  }
+  if ( randomQuote.tags ) {
+    html += `<span class="tags">, ${randomQuote.tags}</span>`
+  }
+
+  html += `</p>`
+
+  // console.log(html)
+
+  document.getElementById('quote-box').innerHTML = html; 
+
+}
 
 
 /***
